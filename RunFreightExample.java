15b42db8270554bc620538f3ffa2df689d9878d7
Duc Hoang Viet <109743947+DucDaiCa@users.noreply.github.com>
2022-10-09 20:30:32 +0300

    Create RunFreightExample.java

---
+++ RunFreightExample.java
@@ -0,0 +1,116 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2019 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.codeexamples.extensions.freight;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.contrib.freight.Freight;
+import org.matsim.contrib.freight.FreightConfigGroup;
+import org.matsim.contrib.freight.carrier.CarrierPlanXmlWriterV2;
+import org.matsim.contrib.freight.utils.FreightUtils;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.io.IOUtils;
+import org.matsim.examples.ExamplesUtils;
+
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+
+/**
+ * @see org.matsim.contrib.freight
+ */
+public class RunFreightExample {
+
+	public static void main(String[] args) throws ExecutionException, InterruptedException{
+
+		// ### config stuff: ###
+
+		Config config = ConfigUtils.loadConfig( IOUtils.extendUrl(ExamplesUtils.getTestScenarioURL( "freight-chessboard-9x9" ), "config.xml" ) );
+
+		config.plans().setInputFile( null ); // remove passenger input
+
+		//more general settings
+		config.controler().setOutputDirectory("./output/freight" );
+
+		config.controler().setLastIteration(0 );		// yyyyyy iterations currently do not work; needs to be fixed.  (Internal discussion at end of file.)
+
+		//freight settings
+		FreightConfigGroup freightConfigGroup = ConfigUtils.addOrGetModule( config, FreightConfigGroup.class ) ;
+		freightConfigGroup.setCarriersFile( "singleCarrierFiveActivitiesWithoutRoutes.xml");
+		freightConfigGroup.setCarriersVehicleTypesFile( "vehicleTypes.xml");
+
+		// load scenario (this is not loading the freight material):
+		Scenario scenario = ScenarioUtils.loadScenario( config );
+
+		//load carriers according to freight config
+		FreightUtils.loadCarriersAccordingToFreightConfig( scenario );
+
+		// how to set the capacity of the "light" vehicle type to "1":
+//		FreightUtils.getCarrierVehicleTypes( scenario ).getVehicleTypes().get( Id.create("light", VehicleType.class ) ).getCapacity().setOther( 1 );
+
+		// output before jsprit run (not necessary)
+		new CarrierPlanXmlWriterV2(FreightUtils.getCarriers( scenario )).write( "output/jsprit_unplannedCarriers.xml" ) ;
+		// (this will go into the standard "output" directory.  note that this may be removed if this is also used as the configured output dir.)
+
+		// Solving the VRP (generate carrier's tour plans)
+		FreightUtils.runJsprit( scenario );
+
+		// Output after jsprit run (not necessary)
+		new CarrierPlanXmlWriterV2(FreightUtils.getCarriers( scenario )).write( "output/jsprit_plannedCarriers.xml" ) ;
+		// (this will go into the standard "output" directory.  note that this may be removed if this is also used as the configured output dir.)
+
+		// ## MATSim configuration:  ##
+		final Controler controler = new Controler( scenario ) ;
+		Freight.configure( controler );
+
+		// otfvis (if you want to use):
+//		OTFVisConfigGroup otfVisConfigGroup = ConfigUtils.addOrGetModule( config, OTFVisConfigGroup.class );
+//		otfVisConfigGroup.setLinkWidth( 10 );
+//		otfVisConfigGroup.setDrawNonMovingItems( false );
+//		config.qsim().setTrafficDynamics( QSimConfigGroup.TrafficDynamics.kinematicWaves );
+//		config.qsim().setSnapshotStyle( QSimConfigGroup.SnapshotStyle.kinematicWaves );
+//		controler.addOverridingModule( new OTFVisLiveModule() );
+
+		// ## Start of the MATSim-Run: ##
+		controler.run();
+	}
+
+	// yyyy I think that having a central freight StrategyManager would be better than the current approach that builds an ad-hoc such
+	// strategy manager, with the strategies hardcoded there as well.  I currently see two approaches:
+
+	// (1) freight strategy manager separate from  standard strategy manager.
+
+	// (2) re-use standard strategy manager.
+
+	// Advantage of (2) would be that we could re-use the existing strategy manager infrastructure, including the way it is configured.
+	// Disadvantage would be that the "freight" subpopulation (or maybe even freight*) would be an implicitly reserved keyword.  Todos for this path:
+
+	// * remove deprecated methods from StrategyManager so that it becomes shorter; make final; etc. etc.
+
+	// * try to completely remove StrategyManager and replace by GenericStrategyManager.  Since only then will it accept Carriers and CarrierPlans.
+
+	// Note that the freight strategy manager operates on CarrierPlans, which include _all_ tours/drivers.  One could also have drivers optimize
+	// individually.  They are, however, not part of the standard population.
+
+	// kai, jan'22
+
+
+}
